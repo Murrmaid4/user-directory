9ea5265a17b11586b527201f82209ef2253bf96c
Brandi Murray <pritchettb14@gmail.com>
2021-05-21 22:11:23 -0400

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export class Header extends Component {
         return (
             <div className="header">
                 <h1>Employee Directory</h1>
-                <p>click the carrots to filter by heading or use the search box to narrow your results.
+                <p>click the carrots to filter by first name or use the search box to narrow your results.
                 </p>
             </div>
         )
